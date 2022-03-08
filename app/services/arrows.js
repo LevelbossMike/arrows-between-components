7de4f1e04fe5452e5f840a4eabc77888b32e801a
Michael Klein <michael@firstiwaslike.com>
2022-03-08 18:16:25 +0100

    chore: create helper function to calculate paths between divs

--- app/services/arrows.js
+++ app/services/arrows.js
@@ -2,6 +2,36 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { schedule } from '@ember/runloop';
+import { getBoxToBoxArrow } from 'curved-arrows';
+
+function boxToArrow(ra, rb) {
+  const bbA = ra.getBoundingClientRect();
+  const bbB = rb.getBoundingClientRect();
+
+  const [sx, sy, c1x, c1y, c2x, c2y, ex, ey, ae, as] = getBoxToBoxArrow(
+    bbA.x,
+    bbA.y,
+    bbA.width,
+    bbA.height,
+    bbB.x,
+    bbB.y,
+    bbA.width,
+    bbB.height
+  );
+
+  return {
+    sx,
+    sy,
+    c1x,
+    c1y,
+    c2x,
+    c2y,
+    ex,
+    ey,
+    ae,
+    as,
+  };
+}
 
 export default class ArrowsService extends Service {
   @tracked evals = [];
